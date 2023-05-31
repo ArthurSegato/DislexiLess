d987c5ffb6d8b29a8654408e4eb5ae57a113fea1
Arthur Segato <arthursegato@outlook.com>
2023-05-31 17:31:43 -0300

    email bug

--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -117,6 +117,7 @@ import { required, email, minLength, helpers } from '@vuelidate/validators'
 import { reactive, computed } from 'vue'
 import { useFetch } from '@vueuse/core'
 import useVuelidate from '@vuelidate/core'
+import router from '@/router'
 
 const api_path = import.meta.env.VITE_API_PATH;
 
@@ -138,9 +139,12 @@ const rules = computed(() => {
 
 const v$ = useVuelidate(rules, formData)
 
-const { execute, isFetching, error, statusCode } = useFetch(`${api_path}/auth/signup`, {
+const { execute, isFetching, error, statusCode, onFetchResponse } = useFetch(`${api_path}/auth/signup`, {
   immediate: false
 })
   .post(formData)
   .json()
+onFetchResponse((response) => {
+  router.push({ name: 'signin'});
+})
 </script>
